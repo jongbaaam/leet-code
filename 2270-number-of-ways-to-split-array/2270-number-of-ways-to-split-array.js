7888b155e5780083ced8c1f756d78fe1f06f1fa6
JB <jongbaaam@gmail.com>
2025-01-06 19:04:56 +0900

    Time: 10 ms (83.21%), Space: 63.8 MB (44.88%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.js
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.js
@@ -4,27 +4,15 @@
  */
 var waysToSplitArray = function(nums) {
     let validSplitCount = 0;
+    let leftSumValue = 0;
+    let totalSumValue = nums.reduce((acc, cur) => acc + cur, 0);
 
-    const prefixObj = nums.reduce((acc, cur, index) => {
-        acc.totalSumValue += cur;
-
-        if (index !== nums.length - 1) {
-            acc.prefixSumValue.push(acc.totalSumValue);
-        }
-        
-        return acc;
-    }, {
-        totalSumValue: 0,
-        prefixSumValue: [],
-    });
-
-    const { totalSumValue, prefixSumValue } = prefixObj;
-    
-
-    console.log(prefixSumValue)
 
     for (let i = 0; i < nums.length - 1; i++) {
-        if (totalSumValue - prefixSumValue[i] <= prefixSumValue[i]) {
+        leftSumValue += nums[i];
+        totalSumValue -= nums[i];
+
+        if (leftSumValue >= totalSumValue) {
             validSplitCount++;
         }
     }    
